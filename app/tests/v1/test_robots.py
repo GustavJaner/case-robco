ff04bda73de04db8595d437da5c3b1f400d82f20
GustavJaner <gustav.janer@gmail.com>
2025-06-01 23:16:10 +0200

    Update FastAPI tests

--- app/tests/v1/test_robots.py
+++ app/tests/v1/test_robots.py
@@ -31,18 +31,18 @@ def test_create_robot_endpoint():
   assert response.status_code == 200
   data = response.json()
   assert data["status"] == "ok"
-  assert data["robot_new"]["name"] == robot_new["name"]
+  assert data["data"]["robot_new"]["name"] == robot_new["name"]
 
 def test_read_robots_endpoint():
   response = client.get("/api/v1/robots")
   assert response.status_code == 200
   data = response.json()
   assert data["status"] == "ok"
-  assert len(data["robots"]) == 1  # Assuming we have one robot created in the previous test.
+  assert len(data["data"]["robots"]) == 1  # Assuming we have one robot created in the previous test.
 
 def test_update_robot_endpoint():
   data = client.get("/api/v1/robots").json()
-  robot_id = data["robots"][0]["id"]  # Get the ID of the first robot (To update it).
+  robot_id = data["data"]["robots"][0]["id"]  # Get the ID of the first robot (To update it).
   assert robot_id is not None
 
   robot_updated_config = {
@@ -56,5 +56,5 @@ def test_update_robot_endpoint():
   assert response.status_code == 200
   data = response.json()
   assert data["status"] == "ok"
-  assert data["robot_updated"]["id"] == robot_id, "Robot ID should not change"
-  assert data["robot_updated"]["name"] == robot_updated_config["name"]
+  assert data["data"]["robot_updated"]["id"] == robot_id, "Robot ID should not change"
+  assert data["data"]["robot_updated"]["name"] == robot_updated_config["name"]
